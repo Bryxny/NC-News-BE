8e49d2004bc30720117ccb837f70fe5f0786391f
Bryxny <bryonydoh2@gmail.com>
2025-04-28 15:28:33 +0100

    4-get-articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -88,3 +88,28 @@ describe("GET /api/articles/:article_id", () => {
       });
   });
 });
+
+describe("GET /api/articles", () => {
+  test("200: Responds with an array of article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles.length).toEqual(data.articleData.length);
+        const dates = body.articles.map((article) => article.created_at);
+        expect(dates).toBeSorted({ descending: true });
+        body.articles.forEach((article) => {
+          expect(article).toMatchObject({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+});
--- app/app.models.js
+++ app/app.models.js
@@ -20,3 +20,26 @@ exports.selectArticleById = (article_id) => {
       return result.rows[0];
     });
 };
+
+exports.selectArticles = () => {
+  return db
+    .query(`SELECT * FROM articles ORDER BY created_at DESC`)
+    .then(({ rows }) => {
+      const addCommentCount = rows.map((article) => {
+        return countComments(article.article_id).then((count) => {
+          article.comment_count = count;
+          delete article.body;
+          return article;
+        });
+      });
+      return Promise.all(addCommentCount);
+    });
+};
+
+const countComments = (article_id) => {
+  return db
+    .query("SELECT * FROM comments WHERE article_id = $1", [article_id])
+    .then((result) => {
+      return result.rows.length;
+    });
+};
