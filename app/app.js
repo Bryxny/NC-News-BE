17595264d32cf28b75548976f258f7cf5222ff9e
Bryxny <bryonydoh2@gmail.com>
2025-05-19 09:50:58 +0100

    updated dependencies

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
-const { badRequest, customError } = require("./app.errors");
+const { badRequest, customError } = require('./app.errors');
 
-const apiRouter = require("./routes/api-router");
+const apiRouter = require('./routes/api-router');
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
-app.all("/*splat", (req, res) => {
-  res.status(404).send({ msg: "Not Found" });
+app.all('/*splat', (req, res) => {
+  res.status(404).send({ msg: 'Not Found' });
 });
 
 app.use(badRequest);
